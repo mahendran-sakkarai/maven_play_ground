59895b65731a182a982a3363058284fd90988a2a
Mahendran Sakkarai <mahendranmails@gmail.com>
2020-04-26 14:45:16 +0530

    Tried with maven-archetype-quickstart

---
+++ 1_maven_quickstart/MavenQuickStartGenerator/src/main/java/com/github/mahendran_sakkarai/App.java
@@ -0,0 +1,13 @@
+package com.github.mahendran_sakkarai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
